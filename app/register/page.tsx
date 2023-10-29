00b120c8fef684db0d6bc58a401aedbfee050182
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2023-10-29 17:11:49 +0530

    minor changes

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -32,7 +32,7 @@ export default function Register() {
           </div>
         </div>
       </div>
-      <div className="shadow-lg absolute left-36 top-4 w-2/6 border-lightGray border-[1px] rounded-[34px]">
+      <div className="shadow-lg absolute left-36 top-6 w-2/6 border-lightGray border-[1px] rounded-[34px]">
         <form
           onSubmit={(e) => {
             e.preventDefault();
